f5ea47229d09c785f55211520ccfcef9cfbf0c6c
Justin Chrysler <pintodragon@gmail.com>
2018-11-10 22:43:09 -0500

    Added P05

--- src/main/scala/com/pintodragon/P05.scala
+++ src/main/scala/com/pintodragon/P05.scala
@@ -1,5 +1,17 @@
 package com.pintodragon
 
-object P05 {
+import scala.annotation.tailrec
 
+object P05 {
+  def reverse[T](list: List[T]): List[T] = list.reverse
+
+  def reverseRec[T](list: List[T]): List[T] = {
+    @tailrec
+    def rec(list: List[T], dest: List[T]): List[T] = list match {
+      case x :: Nil => x +: dest
+      case x :: xs => rec(xs, x +: dest)
+      case _ => throw new NoSuchElementException
+    }
+    rec(list, List())
+  }
 }
--- src/test/scala/com/pintodragon/P05Test.scala
+++ src/test/scala/com/pintodragon/P05Test.scala
@@ -3,5 +3,11 @@ package com.pintodragon
 import org.scalatest.{FunSpec, Matchers}
 
 class P05Test extends FunSpec with Matchers {
+  it("should reverse the list") {
+    P05.reverse(List(1,2,3,4,5)) should be(List(5,4,3,2,1))
+  }
 
+  it("Rec: should reverse the list") {
+    P05.reverseRec(List(1,2,3,4,5)) should be(List(5,4,3,2,1))
+  }
 }
